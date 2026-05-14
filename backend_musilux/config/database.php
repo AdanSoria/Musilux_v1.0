95b05c637e1719db09b913b0ecfc21c43a6ec78a
AdanSoria <joadsoriach@ittepic.edu.mx>
2026-05-13 21:43:10 -0700

    Sirbe please v2

--- backend_musilux/config/database.php
+++ backend_musilux/config/database.php
@@ -58,17 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_merge(
-                array_filter([
-                    (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env(
-                        'MYSQL_ATTR_SSL_CA',
-                        env('APP_ENV') === 'production' ? '/etc/ssl/certs/ca-certificates.crt' : null
-                    ),
-                ]),
-                env('APP_ENV') === 'production' || env('MYSQL_ATTR_SSL_CA') ? [
-                    (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_VERIFY_SERVER_CERT : \PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT) => false,
-                ] : []
-            ) : [],
+            'options' => extension_loaded('pdo_mysql') ? [
+                PDO::MYSQL_ATTR_GET_SERVER_PUBLIC_KEY  => true,
+                PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+            ] : [],
         ],
 
         'mariadb' => [
